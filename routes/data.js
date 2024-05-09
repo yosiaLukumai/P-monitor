0e8bd316f63b6bcefda3155985e10d232426c38e
yosiaLukumai <yosialukumai@gmail.com>
2024-05-09 03:14:45 +0300

    route for fetching sizes

--- routes/data.js
+++ routes/data.js
@@ -6,6 +6,7 @@ const dataRoutes = (app) => {
     router.get("/retrieve/:id", dataController.FindLastData)
     router.get("/specific/:parameter/:id", dataController.fetchDataLogs)
     router.get("/graphdata/:deviceId", dataController.serveGraphData)
+    router.get("/graphdata/sizes/:deviceId", dataController.FindSizes)
     return app.use("/data",router)
 }
 
